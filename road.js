6e5c3e577a3145945127f537dd23c6f446e31a58
Kristina <kristinashrestha2055@gmail.com>
2025-02-13 00:22:47 -0500

    adding borders to the lane

--- road.js
+++ road.js
@@ -9,20 +9,48 @@ class Road {
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
+  }
+
+  getLaneCenter(laneIndex) {
+    const lineWidth = this.width / this.laneCount;
+    return (
+      this.left +
+      lineWidth / 2 +
+      Math.min(laneIndex, this.laneCount - 1) * lineWidth
+    );
   }
 
   draw(ctx) {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
+
+      ctx.setLineDash([30, 30]);
 
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 }
 
