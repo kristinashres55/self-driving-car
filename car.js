b8b632f9326e50f5dccc6f981194f1a3abeb71ec
Kristina <kristinashrestha2055@gmail.com>
2025-02-25 11:15:35 -0500

    Adidng sensor.js

--- car.js
+++ car.js
@@ -9,11 +9,13 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -69,5 +71,6 @@ class Car {
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
     ctx.restore();
+    this.sensor.draw(ctx);
   }
 }
