648b67e562b60a6b3dd93a9412e6f3910c27d871
Kristina <kristinashrestha2055@gmail.com>
2025-02-25 11:23:38 -0500

    Adding private methid castRays

--- sensor.js
+++ sensor.js
@@ -1,21 +1,27 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 3;
-    this.rayLength = 100;
-    this.raySpeed = Math.PI / 4;
+    this.rayCount = 5;
+    this.rayLength = 150;
+    this.raySpeed = Math.PI / 2;
     // this.rayAngle= [-Math.PI / 4, 0, Math.PI / 4];
     this.rays = [];
     // this.collisions = [];
   }
   update() {
+    this.#castRays();
+  }
+
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
-      const rayAngle = lerp(
-        this.raySpeed / 2,
-        -this.raySpeed / 2,
-        i / (this.rayCount - 1)
-      );
+      const rayAngle =
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle;
+
       const start = { x: this.car.x, y: this.car.y };
       const end = {
         x: this.car.x - Math.sin(rayAngle) * this.rayLength,
@@ -24,11 +30,13 @@ class Sensor {
       this.rays.push([start, end]);
     }
   }
+
   draw(ctx) {
     for (let i = 0; i < this.rayCount; i++) {
       ctx.beginPath();
       ctx.lineWidth = 2;
       ctx.strokeStyle = "yellow";
+
       ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
       ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
       ctx.stroke();
