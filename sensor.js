c0c97d0acedf14546b5dc4354f4b3313c0044c49
Kristina <kristinashrestha2055@gmail.com>
2025-02-25 11:54:18 -0500

    Adding road borders and black stroke in road borders

--- sensor.js
+++ sensor.js
@@ -3,13 +3,42 @@ class Sensor {
     this.car = car;
     this.rayCount = 5;
     this.rayLength = 150;
-    this.raySpeed = Math.PI / 2;
-    // this.rayAngle= [-Math.PI / 4, 0, Math.PI / 4];
+    this.raySpread = Math.PI / 2;
+
     this.rays = [];
-    // this.collisions = [];
+    this.readings = [];
   }
-  update() {
+
+  update(roadBorders) {
     this.#castRays();
+    this.readings = [];
+    for (let i = 0; i < this.rays.length; i++) {
+      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+    }
+  }
+
+  #getReading(ray, roadBorders) {
+    let touches = [];
+
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[i][0],
+        roadBorders[i][1]
+      );
+      if (touch) {
+        touches.push(touch);
+      }
+    }
+
+    if (touches.length == 0) {
+      return null;
+    } else {
+      const offsets = touches.map((e) => e.offset);
+      const minOffset = Math.min(...offsets);
+      return touches.find((e) => e.offset == minOffset);
+    }
   }
 
   #castRays() {
@@ -33,12 +62,23 @@ class Sensor {
 
   draw(ctx) {
     for (let i = 0; i < this.rayCount; i++) {
+      let end = this.rays[i][1];
+      if (this.readings[i]) {
+        end = this.readings[i];
+      }
+
       ctx.beginPath();
       ctx.lineWidth = 2;
       ctx.strokeStyle = "yellow";
-
       ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "black";
+      ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
       ctx.stroke();
     }
   }
